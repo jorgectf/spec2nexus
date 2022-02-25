9daed17aee654276fd8661f346e8faab118ce912
GitHub Action <action@github.com>
2022-02-25 21:58:46 +0000

    Add changes for c42ce62ac6280f509a1ad31c2a37636b5a9b10da

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("spec2nexus 5.gf3a56fa documentation");
+        $("li.nav-item-0 a").text("spec2nexus 7.gc42ce62 documentation");
     }
 });
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.gf3a56fa',
+    VERSION: '7.gc42ce62',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
