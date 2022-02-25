9cd3387930c85c76b0feb95f3154538bc402a60f
GitHub Action <action@github.com>
2022-02-25 20:18:49 +0000

    Add changes for c7d37cbdd725e4ee382bb01131b144e28ac42adc

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("spec2nexus 3.gb75be47 documentation");
+        $("li.nav-item-0 a").text("spec2nexus 4.gc7d37cb documentation");
     }
 });
