94d6f962602231cbebbfe49991a09941870f54bc
GitHub Action <action@github.com>
2022-02-28 06:09:02 +0000

    Add changes for 6057139b38b404ea61f34942cec6796ef7b0c193

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("spec2nexus 27.gea98ad0 documentation");
+        $("li.nav-item-0 a").text("spec2nexus 28.g6057139 documentation");
     }
 });
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '27.gea98ad0',
+    VERSION: '28.g6057139',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
