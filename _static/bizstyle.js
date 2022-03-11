c3f533335c96e244b574930b051f250a7a973350
GitHub Action <action@github.com>
2022-03-11 00:29:12 +0000

    Add changes for 196a5d0b72bece53d35cff40793d40513323209a

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("spec2nexus 29.g1ec3f15 documentation");
+        $("li.nav-item-0 a").text("spec2nexus 29.g196a5d0 documentation");
     }
 });
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '29.g1ec3f15',
+    VERSION: '29.g196a5d0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
